6c77ebb8b0e41a1adbccd75c038ad0e06a858bbc
Fernandabitten <fernandab.bittencourt@gmail.com>
2023-01-01 20:45:47 -0300

    up in jason server

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { createStore } from 'vuex'
 import axios from 'axios'
-
+// http://my-json-server.typicode.com/Fernandabitten/backend-todo-vue3
 export default createStore({
   state: {
     todos: []
@@ -29,27 +29,27 @@ export default createStore({
   },
   actions: {
     getTodos({commit}) {
-      return axios.get('http://localhost:3000/todos')
+      return axios.get('https://my-json-server.typicode.com/Fernandabitten/backend-todo-vue3/todos')
       .then((response) => {
         commit('storeTodos', response.data)
       })
     },
 
     addTodo({commit}, data){
-      return axios.post('http://localhost:3000/todos', data).then((response) => {
+      return axios.post('https://my-json-server.typicode.com/Fernandabitten/backend-todo-vue3/todos', data).then((response) => {
       commit('storeTodo', response.data);
       })     
     },
 
     updateTodo({commit}, {id, data}){
-      return axios.put(`http://localhost:3000/todos/${id}`, data)
+      return axios.put(`https://my-json-server.typicode.com/Fernandabitten/backend-todo-vue3/todos/${id}`, data)
         .then((response) => {
         commit('storeTodo', response.data);
       })
     },
 
     deleteTodo({commit},  id){
-      return axios.delete(`http://localhost:3000/todos/${id}`)
+      return axios.delete(`https://my-json-server.typicode.com/Fernandabitten/backend-todo-vue3/todos/${id}`)
         .then(() => {
         commit('deleteTodo', id);
       })
